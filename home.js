ccff3189938baafb2a7ec7b0b407e92120e3da5d
prakash <56664619+prakashaf@users.noreply.github.com>
2023-06-29 12:50:06 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,77 +1,77 @@
 
-  const btncart = document.querySelector('#btn1');
-  const cart1 = document.querySelector('.cartmv');
-  const btncls = document.querySelector('.cancel');
+const btncart = document.querySelector('#btn1');
+const cart1 = document.querySelector('.cartmv');
+const btncls = document.querySelector('.cancel');
 
-  btncart.addEventListener('click',()=>{
-    cart1.classList.add('cartmv-active');
-  })
-  btncls.addEventListener('click',()=>{
-    cart1.classList.remove('cartmv-active');
-  })
-  
-  var cart = [];
+btncart.addEventListener('click',()=>{
+  cart1.classList.add('cartmv-active');
+})
+btncls.addEventListener('click',()=>{
+  cart1.classList.remove('cartmv-active');
+})
+
+var cart = [];
 var total = 0;
 
 // Add to Cart button click event
 $(".add-to-cart").click(function() {
-  var name = $(this).data("name");
-  var price = Number($(this).data("price"));
-  var item = {
-    name: name,
-    price: price
-  };
-  cart.push(item);
-  total += price;
-  updateCart();
+var name = $(this).data("name");
+var price = Number($(this).data("price"));
+var item = {
+  name: name,
+  price: price
+};
+cart.push(item);
+total += price;
+updateCart();
 });
 
 // Update the cart
 function updateCart() {
-  $(".cart").empty();
-  for (var i = 0; i < cart.length; i++) {
-    var item = cart[i];
-    var html = '<li class="list-group-item d-flex justify-content-between align-items-center">' +
-               item.name +
-               '<span class="badge badge-primary badge-pill">$' + item.price.toFixed(2) + '</span>' +
-               '<button class="btn btn-danger btn-sm remove-from-cart" data-index="' + i + '">Remove</button>' +
-               '</li>';
-    $(".cart").append(html);
-  }
-  $(".total").text("₹" + total.toFixed(2));
-  updateCartCount(); // Update the cart count
+$(".cart").empty();
+for (var i = 0; i < cart.length; i++) {
+  var item = cart[i];
+  var html = '<li class="list-group-item d-flex justify-content-between align-items-center">' +
+             item.name +
+             '<span class="badge badge-primary badge-pill">$' + item.price.toFixed(2) + '</span>' +
+             '<button class="btn btn-danger btn-sm remove-from-cart" data-index="' + i + '">Remove</button>' +
+             '</li>';
+  $(".cart").append(html);
+}
+$(".total").text("₹" + total.toFixed(2));
+updateCartCount(); // Update the cart count
 }
 
 // Update the cart count
 function updateCartCount() {
-  var cartCount = cart.length;
-  $(".cart-count").text(cartCount);
+var cartCount = cart.length;
+$(".cart-count").text(cartCount);
 }
 
 // Remove from Cart button click event
 $(document).on("click", ".remove-from-cart", function() {
-  var index = $(this).data("index");
-  var item = cart[index];
-  total -= item.price;
-  cart.splice(index, 1);
-  updateCart();
+var index = $(this).data("index");
+var item = cart[index];
+total -= item.price;
+cart.splice(index, 1);
+updateCart();
 });
 
 $(".checkout-btn").click(function() {
-  checkout();
+checkout();
 });
 
 function checkout() {
-  if (isLoggedIn == false) {
-    alert("Please log in to proceed with checkout.");
-    return;
-  }
-  if (cart.length === 0) {
-    alert("Your cart is empty!");
-    return;
-  }
+if (isLoggedIn == false) {
+  alert("Please log in to proceed with checkout.");
+  return;
+}
+if (cart.length === 0) {
+  alert("Your cart is empty!");
+  return;
+}
 
-  cart = [];
-  total = 0;
-  updateCart();
+cart = [];
+total = 0;
+updateCart();
 }
